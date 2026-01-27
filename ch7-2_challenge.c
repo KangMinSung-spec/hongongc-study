971e51f559f3dd29332bc3c79e200a65cd9340bb
KangMinSung-spec <kkms0503@naver.com>
2026-01-27 22:28:03 +0900

    Create ch7-2_challenge.c

--- ch7-2_challenge.c
+++ ch7-2_challenge.c
@@ -12,12 +12,10 @@ int main(void)
 
 int rec_func(int n)
 {
-    
-    if (n == 1) 
+     if (n == 1) 
     {
         return 1;
     }
     
-   
-    return n + rec_func(n - 1);
+   return n + rec_func(n - 1);
 }
