24bf7da165b98d3a7bd21e298546e2c3eafb3b36
KangMinSung-spec <kkms0503@naver.com>
2026-03-31 23:30:43 +0900

    Create ex19-10.c-헤더 파일의 중복 포함 문제 해결 방법(Point 구조체 선언, Line 구조체 선언, Point와 Line 구조체 모두 사용)

---
+++ ex19-10.c
@@ -0,0 +1,34 @@
+#ifndef _POINT_H_  //_POINT_H_ 매크로명이 정의되어 있지 않으면
+#define _POINT_H_  //_POINT_H_ 매크로명 정의
+
+typedef struct
+{
+    int x;  //x좌표
+    int y;  //y좌표
+} Point;
+
+#endif  //#ifndef _POINT_H의 끝
+
+#include "point.h"  //Point 구조체를 위해 포함
+
+typedef struct
+{
+    Point first;  //첫 번째 점
+    Point second;  //두 번째 점
+} Line;
+
+#include <stdio.h>
+#include "point.h"  //Point 구조체 선언
+#include "line.h"  //Line 구조체 선언
+
+int main(void)
+{
+    Line a = { {1, 2}, {5, 6} };  //Line 구조체 변수 초기화
+    Point b;  //가운데 점의 좌표 저장
+    
+    b.x = (a.first.x + a.second.x) / 2;  //가운데 점의 x좌표 계산
+    b.y = (a.first.y + a.second.y) / 2;  //가운데 점의 y좌표 계산
+    printf("선의 가운데 점의 좌표 : (%d, %d)\n", b.x, b.y);
+    
+    return 0;
+}
